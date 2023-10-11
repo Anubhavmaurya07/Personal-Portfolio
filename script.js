524f123c5e381ba0f37cfaefa650fea5d7ecb477
Anubhav Maurya <116622420+Anubhavmaurya07@users.noreply.github.com>
2023-10-11 15:35:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+console.log(tablinks);
+
+function opentab(tabname) {
+    for (tab-link of tab-links) {
+        tab-link.classlist.remove("active-link");
+    }
+    for (tab-content of tab-contents) {
+        tab-content.classlist.remove("active-tab");
+    }
+}
